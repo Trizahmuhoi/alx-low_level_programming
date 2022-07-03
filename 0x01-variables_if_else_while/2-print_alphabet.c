723878f8b8aa51a147b0dcb15ec57db7d6c2e038
Trizahmuhoi <muhoiteresiah@gmail.com>
2022-07-02 21:30:43 -0700

    My c variables project

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include<studio.h>
+/**
+ * main - entry point
+ *
+ * Return: always returns 0
+ */
+int main(void)
+{
+char a;
+for (a = 'a'; a <= 'z' ; a++)
+putchar(a);
+putchar('\n');
+return (0);
+}
