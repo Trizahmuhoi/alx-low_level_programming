b413325bdbc5c5853c03803883da64f9df40e393
Trizahmuhoi <muhoiteresiah@gmail.com>
2022-07-06 12:33:23 -0700

    My hot seven fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,14 +2,18 @@
 
 /**
  * print_last_digit -> prints the last digit
- * @n: the arguments
- * Return: the last digit
+ * @n: n is an integer
+ * Description: prints the last digit of a number
+ * Return: integer
  */
 int print_last_digit(int n)
 {
+int a;
 if (n < 0)
 n = -n;
-x = n % 10;
+a = n % 10;
+if (a < 0)
+a = -a;
 _putchar(x + '0')
-return (n % 10);
+return (a);
 }
