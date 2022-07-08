d9eafe1c6b574b907d36994f45e6b99719c09e17
Trizahmuhoi <muhoiteresiah@gmail.com>
2022-07-08 13:30:10 -0700

    Tasktwo fix

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,17 @@
 #include<studio.h>
 /**
- * main - entry point
- *
+ * main -> assign a random number to the variable n each time it is executed
+ * and prints the last digit of the number stored in the variable n
  * Return: always returns 0
  */
 int main(void)
 {
-char a;
-for (a = 'a'; a <= 'z' ; a++)
-putchar(a);
-putchar('\n');
+int ch;
+for (ch = 97; ch <= 122; ch++)
+{
+putchar(ch);
+}
+putchar(10);
+
 return (0);
 }
