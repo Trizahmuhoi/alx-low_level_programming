1693f4eeb9e70d64f95e0989ed19eb5302e4c4a2
Trizahmuhoi <muhoiteresiah@gmail.com>
2022-07-07 20:36:09 -0700

    My third task fix

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,14 @@
 #include "main.h"
 
 /**
- * print_numbers - print numbers
- * Return: Always 0
- */
+* print_numbers -> print numbers
+*/
+
 void print_numbers(void)
 {
-int i;
-i = 0;
-while (i < 10)
-{
-_putchar (i + '0');
-1++;
-}
-_putchar ('\n');
+int x;
+
+for (x = 0; x < 10; x++)
+_putchar(x + '0');
+_putchar('\n');
 }
