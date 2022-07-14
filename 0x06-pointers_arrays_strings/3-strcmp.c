60e7285b79746782635c2682e42d8cad9ee45d5b
Trizahmuhoi <muhoiteresiah@gmail.com>
2022-07-13 20:24:35 -0700

    My 3 task

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * _strncpy - C function that copies a string
- * @des: buffer storing the string copy
- * @src: the source string
- * *n: number maximun copied
- * Return: returns
+ * _strcmp - compare string values
+ * @s1: input value
+ * @s2: input value
+ *
+ * Return: s1[i] - s2[i]
  */
 
 int _strcmp(char *s1, char *s2)
@@ -12,7 +12,7 @@ int _strcmp(char *s1, char *s2)
 int i;
 
 i = 0;
-while (s1[i] != '\0' && s2[i] != '\0')
+while (s1[i] != ('\0') && s2[i] != ('\0'))
 {
 if (s1[i] != s2[i])
 {
