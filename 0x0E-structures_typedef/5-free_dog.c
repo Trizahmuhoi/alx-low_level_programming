49306f59956c8cbec4ad57ec297f0904f519a98b
Trizahmuhoi <muhoiteresiah@gmail.com>
2022-08-01 21:54:33 +0300

    Write a function that frees dogs

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -3,14 +3,14 @@
 
 /**
  * free_dog - frees memory allocated for a struct dog
- * @o: struct dog to free
+ * @d: struct dog to free
  */
 void free_dog(dog_t *d)
 {
-if (o)
+if (d)
 {
-free(o->name);
-free(o->owner);
-free(o);
+free(d->name);
+free(d->owner);
+free(d);
 }
 }
